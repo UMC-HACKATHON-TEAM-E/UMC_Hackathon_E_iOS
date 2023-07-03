3a1711bb6ff2e98784e70787392b26252548e4ac
JEONG-J <ikejhc159@gmail.com>
2023-07-04 03:18:03 +0900

    Final Detail

--- UMC-Hackathon/Views/GraphView.swift
+++ UMC-Hackathon/Views/GraphView.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import Hue
-
 class GraphView: UIView {
     
     private var outerRingColor: UIColor = .systemGreen
@@ -20,6 +19,34 @@ class GraphView: UIView {
         super.draw(rect)
         drawGraph()
     }
+/*
+    private func drawGraph() {
+        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        let radius = min(bounds.width, bounds.height) / 2 - 10
+        
+       
+        let outerRingPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        outerRingColor.setStroke()
+        outerRingPath.lineWidth = 20
+        outerRingPath.stroke()
+        
+        
+        
+        let innerRingPath = UIBezierPath(arcCenter: center, radius: radius - 30, startAngle: 0, endAngle: 2 * CGFloat.pi * innerRingPercentage, clockwise: true)
+        innerRingColor.setStroke()
+        innerRingPath.lineWidth = 20
+        innerRingPath.stroke()
+       
+        /*
+        let outerRingCoverPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi * outerRingPercentage, clockwise: true)
+        UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00).setStroke()
+        UIColor.systemCyan.setStroke()
+        outerRingCoverPath.lineWidth = 20
+        outerRingCoverPath.stroke()
+         */
+ 
+ */
+    
     
     private func drawGraph() {
         let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
@@ -30,17 +57,31 @@ class GraphView: UIView {
         outerRingPath.lineWidth = 20
         outerRingPath.stroke()
         
-        let innerRingPath = UIBezierPath(arcCenter: center, radius: radius - 30, startAngle: 0, endAngle: 2 * CGFloat.pi * innerRingPercentage, clockwise: true)
+        let innerRingPath = UIBezierPath(arcCenter: center, radius: radius - 20, startAngle: 0, endAngle: 2 * CGFloat.pi * innerRingPercentage, clockwise: true)
         innerRingColor.setStroke()
         innerRingPath.lineWidth = 20
         innerRingPath.stroke()
         
-        let outerRingCoverPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi * outerRingPercentage, clockwise: true)
-        UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00).setStroke()
-        UIColor.systemCyan.setStroke()
-        outerRingCoverPath.lineWidth = 20
-        outerRingCoverPath.stroke()
+        let outerRingCoverPath = CGMutablePath()
+        outerRingCoverPath.addArc(center: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi * outerRingPercentage, clockwise: false)
+        outerRingCoverPath.addArc(center: center, radius: radius - 20, startAngle: 2 * CGFloat.pi * outerRingPercentage, endAngle: 0, clockwise: true)
+        
+        let shapeMask = CAShapeLayer()
+        shapeMask.path = outerRingCoverPath
+        shapeMask.fillRule = .evenOdd
+        
+        let gradientLayer = CAGradientLayer()
+       // gradientLayer.frame = CGRect(x: 0, y: 10, width: self.bounds.width, height: self.bounds.height)
+        gradientLayer.frame = self.bounds
+        gradientLayer.colors = [UIColor(red: 0.00, green: 0.48, blue: 1.00, alpha: 1.00).cgColor, UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00).cgColor]
+
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        gradientLayer.mask = shapeMask
+        self.layer.addSublayer(gradientLayer)
     }
+
     
     func setOuterRingColor(_ color: UIColor) {
         outerRingColor = color
@@ -61,6 +102,4 @@ class GraphView: UIView {
         innerRingPercentage = max(0.0, min(1.0, percentage))
         setNeedsDisplay()
     }
-    
 }
-
