c55a2fd7321e4e2318ca016a7e220154d9396e1c
Jeong Dong <eejxdong99@gmail.com>
2023-07-03 19:47:11 +0900

    feat: 홈 화면 틀 구성

---
+++ UMC-Hackathon/Controllers/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  UMC-Hackathon
+//
+//  Created by 이정동 on 2023/07/03.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let flowLayout = UICollectionViewFlowLayout()
+    
+    let data: [Int] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupCollectionView()
+    }
+
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        flowLayout.scrollDirection = .vertical
+        flowLayout.itemSize = CGSize(width: view.frame.width, height: 350)
+        collectionView.collectionViewLayout = flowLayout
+    }
+
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as! HomeCollectionViewCell
+        return cell
+    }
+    
+    
+}
---
+++ UMC-Hackathon/Views/HomeCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCollectionViewCell.swift
+//  UMC-Hackathon
+//
+//  Created by 이정동 on 2023/07/03.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var first: UIView!
+    
+    @IBOutlet weak var second: UIView!
+    
+    @IBOutlet weak var third: UIView!
+    
+    @IBOutlet weak var fourth: UIView!
+    
+    @IBOutlet weak var fifth: UIView!
+}
