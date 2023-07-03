80ba5ccf2eff94fcbd2855f72d4d58063a0d212b
JEONG-J <ikejhc159@gmail.com>
2023-07-03 20:32:32 +0900

    Hue

--- UMC-Hackathon/Controllers/DetailViewController.swift
+++ UMC-Hackathon/Controllers/DetailViewController.swift
@@ -6,24 +6,18 @@
 //
 
 import UIKit
+import Hue
 
 class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //main
+        let gradientLayer = CAGradientLayer()
+        let gradient = [UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00), UIColor(red: 0.00, green: 0.48, blue: 1.00, alpha: 1.00)].gradient()
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
